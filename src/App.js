c2157b62f63c63e897adf6a69cd5d565c0e70367
Visal20497 <visal20497@gmail.com>
2024-02-19 01:10:41 +0530

    redux toolkit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+ import React from 'react'
+import Todo from './Todo'
+import Count from './Count'
+import FetchData from './FetchData'
+ 
+ function App() {
+   return (
+     <div>
+      <Count/>
+      <hr/>
+      <FetchData/>
+      <hr/>
+      <Todo/>
+     </div>
+   )
+ }
+ 
+ export default App
